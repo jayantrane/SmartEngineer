ad781721c5001f37cbf73cd365f12b0741f3ae0f
jayantrane <jayantrane0811@gmail.com>
2017-10-02 12:25:17 +0530

    2nd commit

--- app/src/main/java/com/vjti/dj/smartengineer/ReminderAdapter.java
+++ app/src/main/java/com/vjti/dj/smartengineer/ReminderAdapter.java
@@ -22,7 +22,7 @@ public class ReminderAdapter  extends ArrayAdapter<Reminder> implements View.OnC
 
     private static class ViewHolder {
         TextView txtName;
-        TextView txtType;
+        TextView txtDate;
         ImageView info;
     }
 
@@ -60,7 +60,7 @@ public class ReminderAdapter  extends ArrayAdapter<Reminder> implements View.OnC
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.list_reminder, parent, false);
             viewHolder.txtName = (TextView) convertView.findViewById(R.id.name1);
-            viewHolder.txtType = (TextView) convertView.findViewById(R.id.date1);
+            viewHolder.txtDate = (TextView) convertView.findViewById(R.id.date1);
             viewHolder.info = (ImageView) convertView.findViewById(R.id.item_info);
 
             result=convertView;
@@ -72,7 +72,7 @@ public class ReminderAdapter  extends ArrayAdapter<Reminder> implements View.OnC
         }
 
         viewHolder.txtName.setText( reminder.getName());
-        viewHolder.txtName.setText( reminder.getDate());
+        viewHolder.txtDate.setText( reminder.getDate());
         viewHolder.info.setOnClickListener(this);
         viewHolder.info.setTag(position);
         return convertView;
